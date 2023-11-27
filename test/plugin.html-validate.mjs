c2c394068d17bd6a939ba173a035dcb2183ccf33
William Entriken <github.com@phor.net>
2023-11-27 16:55:15 -0500

    all mjs

---
+++ test/plugin.html-validate.mjs
@@ -0,0 +1,18 @@
+import { definePlugin } from "html-validate";
+import { MailtoAwesomeRule } from "./plugin.html-validate.mailto-awesome.mjs";
+
+const plugin = definePlugin({
+  name: "pacific-medical-training",
+  rules: {
+    "mailto-awesome": MailtoAwesomeRule,
+   },
+  configs: {
+    recommended: {
+      rules: {
+        "pacific-medical-training/mailto-awesome": "error",
+      },
+    },
+  },
+});
+
+export default plugin;
